11b5ee913a26d39156ac0efbc92a99451ac96bd8
nitram2211 <mtzenev@gmail.com>
2014-12-03 02:03:58 +0200

    initial commit
    zadacha 6 mi e zadacha 5 oburkah pri sazdavaneto na projecta

--- zad6/src/zad6/zad6.java
+++ zad6/src/zad6/zad6.java
@@ -6,28 +6,25 @@ public class zad6 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Type a number:");
 		int n = input.nextInt();
-		
-		if(n<=3){
-			n= n*5;
-			System.out.println("your score is:" +n);
+		if (n < 1 || n > 9) {
+			System.out.println("Eror.");
 		}
-		else if(n<=6 && n>=4 ){
-			n= n*10;
-			System.out.println("your score is:" +n);
-			
+		if (n <= 3 && n >= 1) {
+			n = n * 5;
+			System.out.println("your score is:" + n);
+		} else if (n <= 6 && n >= 4) {
+			n = n * 10;
+			System.out.println("your score is:" + n);
+
 		}
-		if(n<=9 && n>=7){
-			n= n*50;
-			System.out.println("your score is:" +n);
-		}
-		if(n<=0){
-			System.out.println("Invalid number");
+		if (n <= 9 && n >= 7) {
+			n = n * 50;
+			System.out.println("your score is:" + n);
 		}
 
-		
 	}
 
 }
